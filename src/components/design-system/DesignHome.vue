ba065369610f6478e8d9091a867757312e5a1f0f
aj <art@ampsire.com>
2022-01-17 06:11:58 -0500

    scaffolding

--- src/components/design-system/DesignHome.vue
+++ src/components/design-system/DesignHome.vue
@@ -5,17 +5,20 @@
   <div>
     <h1>Sample Application</h1>
     <SampleApp />
+    <BaseTitle msg="Base Title component" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import SampleApp from "./design-sample/SampleApp.vue";
+import BaseTitle from "./base/BaseTitle.vue";
 
 export default defineComponent({
   name: "DesignHome",
   components: {
     SampleApp,
+    BaseTitle,
   },
   setup() {
     //   add code
